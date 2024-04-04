8e0e6bc776055da542a8b0c22dff98d3528578f9
TO <th.theophyl.hoguet@gmail.com>
2024-04-04 20:59:32 +0200

    WIP tesseract, working CSV

--- src/screens/unities.js
+++ src/screens/unities.js
@@ -2,10 +2,10 @@ import { Dimensions } from "react-native";
 const { height, width } = Dimensions.get("window");
 
 const unities = {
-  fullWidth: width,
-  fullHeight: height,
-  cameraHeight: height * 0.6,
-  topSection: height * 0.2,
-  bottomSection: height * 0.2,
+	fullWidth: width,
+	fullHeight: height,
+	cameraHeight: height * 0.6,
+	topSection: height * 0.2,
+	bottomSection: height * 0.2,
 };
 export default unities;
